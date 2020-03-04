a3ab48fcee552a5ca8d67356f1c8b822d1d6ac57
hkfuertes <hkfuertes@gmail.com>
2020-03-04 18:46:17 +0100

    added date to mock data

--- tension-app-server/createdb.py
+++ tension-app-server/createdb.py
@@ -10,9 +10,9 @@ with app.test_request_context():
     db.create_all()
 
     doctor = Doctor(email="hkfuertes@gmail.com", password="gorilafeliz", name="Miguel", lastName="Fuertes")
-    doctor.id = 1;
-    patient = Patient(doctor=doctor, name='Javier', lastName='Fuertes', gender='male')
-    patient.id = 1;
+    doctor.id = 1
+    patient = Patient(doctor=doctor, name='Javier', lastName='Fuertes', gender='male', birthday='2020-03-03')
+    patient.id = 1
     pressure = Pressure(patient, None, 70, 120, 90)
     weight = Weight(patient, None, 120.6)
 
